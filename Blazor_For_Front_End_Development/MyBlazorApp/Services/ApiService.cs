2f91a81c91d141bd5fe10de910a22bf40e2b772c
sreiswig <sam.reiswig@gmail.com>
2025-02-26 21:09:17 -0600

    Blazor Frontend

---
+++ Blazor_For_Front_End_Development/MyBlazorApp/Services/ApiService.cs
@@ -0,0 +1,15 @@
+using Microsoft.Extensions.Configuration;
+
+public class ApiService
+{
+  private readonly IConfiguration _configuration;
+  
+  public ApiService(IConfiguration configuration) {
+    _configuration = configuration;
+  }
+
+  public string GetApiUrl() {
+    return _configuration["ApiSettings:BaseUrl"];
+  }
+}
+
